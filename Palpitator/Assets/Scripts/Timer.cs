8deea4e44800517c0f544ea4e6d1cb040f8d18ff
Beroux <31445450+Beroux@users.noreply.github.com>
2017-12-08 20:35:31 -0500

    Multiplyer Added after 30 seconds

--- Palpitator/Assets/Scripts/Timer.cs
+++ Palpitator/Assets/Scripts/Timer.cs
@@ -2,23 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour {
-    public Text timerText;
-    private float startTime;
+	public string levelToLoad;
+	public float startTime = 60f;
+	private Text timerText;
 
 	// Use this for initialization
 	void Start () {
-        startTime = Time.time;
+		timerText = GetComponent<Text> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float t = Time.time - startTime;
-
-        string minutes = ((int) t / 60).ToString();
-        string seconds = (t % 60).ToString("f0");
-
-        timerText.text = minutes + ":" + seconds;
+		startTime -= Time.deltaTime;
+		timerText.text = startTime.ToString ("f0");
+			if (startTime <= 0)
+			{
+			SceneManager.LoadScene (levelToLoad);
+		}
 	}
 }
